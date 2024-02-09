5dc4f510301ecb0a42401e4f2e8bf5346b9701ab
jrgrant560 <jrgrant560@gmail.com>
2024-02-09 13:03:12 -0500

    updated Clerk settings

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,14 @@ import { authMiddleware } from "@clerk/nextjs";
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({});
+export default authMiddleware({
+    // publicRoutes: ["/"],
+
+    // This function is called after the user is authenticated
+    afterAuth(auth, req, evt) {}
+});
+
+console.log("Clerk middleware is running!");
  
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
