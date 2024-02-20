aee29e7ab973da9a200a45d8f799d9a70a43a913
jrgrant560 <jrgrant560@gmail.com>
2024-02-20 12:47:23 -0500

    added Upstash ratelimit to limit frequency of user posts

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -4,10 +4,26 @@ import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
 const filterUserForClient = (user: User) => {
 
   return { id: user.id, username: user.username, imageUrl: user.imageUrl };
 }
+
+// Create a new ratelimiter, that allows 3 requests per 1 minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+   * instance with other applications and want to avoid key collisions. The default prefix is
+   * "@upstash/ratelimit"
+   */
+  prefix: "@upstash/ratelimit",
+});
 
 // this file runs on the server end
 
@@ -56,6 +72,10 @@ export const postsRouter = createTRPCRouter({
     )
     .mutation(async ({ ctx, input }) => {
       const authorId = ctx.currentUser.userId!;
+
+      // ratelimit the user
+      const {success} = await ratelimit.limit(authorId);
+      if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS", message: "You are posting too frequently" });
 
       const post = await ctx.db.post.create({
         data: {
